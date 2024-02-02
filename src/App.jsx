6f0228b51098ef0f7ce53f5f4434d465ad77c1b5
monibbormon <monibbormon14@gmail.com>
2024-02-02 17:47:31 +0600

    initial setup of portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      {" "}
+      <h3 className="uppercase text-4xl text-center pt-10">
+        Welcome to new Era of 2024
+      </h3>
+    </div>
+  );
+};
+
+export default App;
